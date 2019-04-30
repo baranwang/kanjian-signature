0a8de1edb441f06dcba80a41beecfb7be563af56
Baran Wang <me@baran.wang>
2019-04-30 10:47:23 +0800

    deploy 命令

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,11 @@ cd dist
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
 
+git init
 git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:baranwang/kanjian-signature.git master:gh-pages
+git push -f https://github.com/baranwang/kanjian-signature.git master:gh-pages
 
 cd -
